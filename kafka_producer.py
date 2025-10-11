58daf16a8d28f71256169af794cae286f4e54ddb
Ejikeomeke <engromeke@gmail.com>
2025-10-11 09:47:42 +0100

    kafka_producer added

---
+++ kafka_producer.py
@@ -0,0 +1,18 @@
+# To stream data to kafka producer
+def stream_data(data):
+    from kafka import KafkaProducer
+    import time
+    import json
+    import logging
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'], max_block_ms=5000)
+    curr_time = time.time()
+    while True:
+            if time.time() > curr_time + 60: #1 minute
+                break
+            try:
+
+                producer.send('omekedata', json.dumps(raw_data).encode('utf-8'))
+            except Exception as e:
+                logging.error(f'An error occured: {e}')
+                continue
+
